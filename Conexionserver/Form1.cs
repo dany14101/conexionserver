30f01119fdd4664e3f7fd4b9c1207da0cdc65711
Daniel <Da@gmail.com>
2025-11-10 06:28:29 -0600

    Actualizar funcion de mensajes

--- Conexionserver/Form1.cs
+++ Conexionserver/Form1.cs
@@ -1,25 +1,15 @@
-﻿using Google.Protobuf.WellKnownTypes;
-using Microsoft.Win32;
-using MySql.Data.MySqlClient;
-using MySqlX.XDevAPI;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static Org.BouncyCastle.Asn1.Cmp.Challenge;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using MySql.Data.MySqlClient;
 namespace Conexionserver
 {
     public partial class Form1 : Form
